edcf91b11b2939cfdd7eb02f5ab329fe69f54d7d
Mohamed Ahmed <52970948+GNMohamed1@users.noreply.github.com>
2023-03-15 00:12:05 +0200

    make the camera shows from the topleft the player

--- code/button.py
+++ code/button.py
@@ -1,7 +1,6 @@
 import pygame
 from settings import *
 
-
 class Button(pygame.sprite.Sprite):
     def __init__(self, text, width, height, pos, func, img_path, font_path):
         super().__init__()
@@ -25,6 +24,7 @@ class Button(pygame.sprite.Sprite):
         self.duration = 200
         self.start_time = -self.duration
         self.cycle = False
+        self.hovered = False
 
     def set_text(self, text):
         self.text = text
@@ -32,12 +32,20 @@ class Button(pygame.sprite.Sprite):
         self.text_rect = self.text_surf.get_rect(center=self.rect.center)
 
     def draw(self, surface):
-        surface.blit(self.img, self.rect)
+        if self.hovered:
+            surface.blit(pygame.transform.scale(self.img, (int(self.rect.width*1.1), int(self.rect.height*1.1))), (self.rect.x-int(self.rect.width*0.05), self.rect.y-int(self.rect.height*0.05)))
+        else:
+            surface.blit(self.img, self.rect)
         surface.blit(self.text_surf, self.text_rect)
 
     def check_mouse(self):
         mouse_pos = pygame.mouse.get_pos()
-        if pygame.mouse.get_pressed()[0] and self.rect.collidepoint(mouse_pos):
+        if self.rect.collidepoint(mouse_pos):
+            self.hovered = True
+        else:
+            self.hovered = False
+
+        if pygame.mouse.get_pressed()[0] and self.hovered:
             self.start_time = pygame.time.get_ticks()
             self.cycle = True
             self.func()
@@ -46,5 +54,5 @@ class Button(pygame.sprite.Sprite):
             self.cycle = False
 
     def update(self):
-        self.draw(self.display_surf)
         self.check_mouse()
+        self.draw(self.display_surf)
